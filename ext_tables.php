471f8c48cab290891a6601ad083e840972029cfb
Mario Matzulla <mario@matzullas.de>
2017-01-31 11:52:17 +0100

    Task: Module Typo3 8 compatible
    Change-Id: Ie29aa3acfab2c69b2cb7e8e013b75a0716aa62f7
    Reviewed-on: https://review.typo3.org/51485
    Reviewed-by: Mario Matzulla <mario@matzullas.de>
    Tested-by: Mario Matzulla <mario@matzullas.de>

--- ext_tables.php
+++ ext_tables.php
@@ -9,5 +9,26 @@ if (TYPO3_MODE=="BE")	{
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule("tools","calicsgenerator","",$extPath."Classes/Backend/Modul/");
 }
 
+if (TYPO3_MODE == "BE") {
+	if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger ( TYPO3_version ) < '8000000') {
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule( "tools", "calicsgenerator", "", $extPath . "Classes/Backend/Modul/" );
+	} else {
+		// Add module
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+				'tools',
+				'txcaldavM1',
+				'',
+				'',
+				[
+						'routeTarget' => \TYPO3\CMS\Caldav\Backend\Modul\IcsGeneratorModul::class . '::mainAction',
+						'access' => 'admin',
+						'name' => 'tools_txcaldavM1',
+						'icon' => 'EXT:' . $_EXTKEY . '/Classes/Backend/Modul/icon_tx_caldav_ics_generator.svg',
+						'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_ics_generator_mod.xml'
+				]
+				);
+	}
+}
+
 
 ?>
