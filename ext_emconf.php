ef8f4b9d1c2bbcdea15b48d20eb26b26c3b25740
Mario Matzulla <mario@matzullas.de>
2017-02-01 10:39:18 +0100

    Testing direct commit and push

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@
  * Extension Manager/Repository config file for ext "caldav".
  *
  * Auto generated 01-07-2012 14:16
- *
+ *test
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
